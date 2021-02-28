99e1bf459f88183588438abd52b0374ff79e0885
Bangyou Zheng <bangyou.zheng@csiro.au>
2021-02-28 11:30:44 +1000

    Update test

--- tests/testthat/test-thermalTime.R
+++ tests/testthat/test-thermalTime.R
@@ -6,9 +6,9 @@ test_that("Thermal time", {
     x_temp <- c(0, 20, 35)
     y_temp <- c(0, 20, 0)
     res <- thermalTimeDaily(mint, maxt, x_temp, y_temp)
-    expect_equal(res, c(15, 13.3333333333))
+    expect_equal(res, c(15, 13.3333333333), tolerance=1e-3)
     res <- thermalTimeDaily(mint, maxt, x_temp, y_temp, method = '3hr')
-    expect_equal(res, c(9.279687499999, 8.7905625000000))
+    expect_equal(res, c(9.279687499999, 8.7905625000000), tolerance=1e-3)
     
     met_file <- system.file("extdata/WeatherHourly.csv", package = "weaana")
     hourly <- read.csv(met_file, as.is = TRUE) %>%
@@ -17,16 +17,16 @@ test_that("Thermal time", {
     x_temp <- c(0, 20, 35)
     y_temp <- c(0, 20, 0)
     res <- thermalTimeHourly(hourly$timestamp, hourly$temperature, x_temp, y_temp)
-    #expect_equal(res$value[1], 16.391951095089993)
+    expect_equal(res$value[1], 16.391951095089993, tolerance=1e-3)
     # Read weather records
     met_file <- system.file("extdata/WeatherRecordsDemo1.met", package = "weaana")
     records <- readWeatherRecords(met_file)
     x_temp <- c(0, 26, 34)
     y_temp <- c(0, 26, 0)
     res <- thermalTime(records, x_temp, y_temp)
-    #expect_equal(res[1,3], 19.549999999)
+    expect_equal(res[1,3], 19.549999999, tolerance=1e-3)
     res <- thermalTime(records, x_temp, y_temp, method = "3hr")
-    #expect_equal(res[1,3], 19.5499999999)
+    expect_equal(res[1,3], 19.5499999999, tolerance=1e-3)
     
       
 })
